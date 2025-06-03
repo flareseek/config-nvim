5db7a98eca1a635f14edef113ea1e05f101e76dc
flareseek <flareseek@gmail.com>
2025-06-04 04:06:07 +0900

    feat: dashboard shortcuts left side

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -4,6 +4,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     confirm_key = { 'o', '<CR>' },
+    shortcuts_left_side = true,
     config = {
       shortcut = {
         { desc = '󰊳 Plugins Update', group = '@property', action = 'Lazy update', key = 'u' },
